16e963678f54b60f2253f863d85890e87771f06b
Nada chaa <140740775+Nada-webdev@users.noreply.github.com>
2024-10-14 23:55:27 +0100

    init

---
+++ paragraphs.js
@@ -0,0 +1,6 @@
+const paragraphs = [
+          "Micro Club is a leading student organization at USTHB, founded in 1985, that fosters innovation and learning in technology and computer science. It offers students workshops, events, and project opportunities in areas like programming, web development, and more",
+          "Design Department: Focuses on creativity and visual communication, helping members master tools like Photoshop, Illustrator, and UI/UX design principles to create compelling graphics, user interfaces, and digital art for various projects.",
+          "Development (Dev) Department: Concentrates on software and web development, teaching members coding skills, best practices, and project development using modern programming languages and frameworks.",
+          "Game Development Department: Specializes in creating video games, where members explore game design, 3D modeling, and programming using platforms like Unity and Unreal Engine to build interactive gaming experiences."
+];
---
+++ script.js
@@ -0,0 +1,131 @@
+const typingText=document.querySelector(".typing-text p")
+const inputFiled=document.querySelector(".wrapper .input-field")
+const mistakeTag=document.querySelector(".mistake span")
+const TimeTag=document.querySelector(".time span b")
+const TryAgainBtn=document.querySelector(".btn .again")
+const NextBtn=document.querySelector(".btn .next")
+let time,
+timeLeft=timeMax=60
+let index=mistakes=StartTimer=0
+function randomPragraph(){
+          let randomIndex= Math.floor(Math.random()* paragraphs.length)
+          typingText.innerHTML="";
+         paragraphs[randomIndex].split("").forEach (letter=>{
+           let letterTag= `<span>${letter}</span>`
+           typingText.innerHTML +=letterTag
+         })
+         TimeTag.innerHTML=timeMax;
+ typingText.addEventListener("click",()=>inputFiled.focus())
+
+}
+
+
+function inputFun(){
+          const char = typingText.querySelectorAll("span");
+          let typedChar = inputFiled.value.split("")[index];
+          
+          if(timeLeft>0){
+            if(!StartTimer){
+              Timing= setInterval(initTime,1000);
+              StartTimer=true;
+            }
+            if(typedChar == null){
+              index--;
+              if(char[index].classList.contains("incorrect")){
+                mistakes --;
+              }
+              char[index].classList.remove("correct", "incorrect");
+            }else{
+  
+              if(char[index].innerText === typedChar){
+                char[index].classList.add("correct")
+      }else{
+               mistakes++;
+               
+                char[index].classList.add("incorrect")
+      }
+      index++
+            }
+
+            mistakeTag.innerHTML=mistakes;
+            char.forEach(span =>span.classList.remove("active"));
+            char[index].classList.add("active")
+          }else{
+            clearInterval(Timing);
+            inputFiled.value="";
+          }
+          
+         
+
+}
+
+function initTime(){
+  if(timeLeft >0){
+timeLeft--;
+TimeTag.innerText=timeLeft;
+  }
+  else{
+    clearInterval(Timing)
+  }
+}
+
+function TryAgain(){
+  
+  timeLeft=timeMax=60
+  index=mistakes=StartTimer=0
+  TimeTag.innerText=timeLeft;
+  mistakeTag.innerHTML=mistakes;
+  inputFiled.value="";
+  clearInterval(Timing);
+
+
+  const char = typingText.querySelectorAll("span");
+
+  char.forEach(span => {
+    span.classList.remove("correct", "incorrect","active");
+  });
+  
+
+
+}
+
+
+function Next() {
+  let finishedAllText = true; 
+  
+  const char = typingText.querySelectorAll("span");
+  
+  
+  char.forEach(span => {
+    if (!span.classList.contains("correct") &&
+        !span.classList.contains("incorrect")) {
+      finishedAllText = false;  
+    }
+  });
+
+ 
+  if (timeLeft > 0 && finishedAllText) {
+   
+    timeLeft = timeMax = 60;
+    index = mistakes = StartTimer = 0;
+
+    
+    TimeTag.innerText = timeLeft;
+    mistakeTag.innerHTML = mistakes;
+    inputFiled.value = "";
+
+  
+    clearInterval(Timing);
+
+    
+    randomPragraph();
+  } else {
+    
+    console.log("You must finish typing the text or time has run out.");
+  }
+}
+
+randomPragraph();
+inputFiled.addEventListener('input', inputFun);
+TryAgainBtn.addEventListener('click', TryAgain);
+NextBtn.addEventListener('click', Next);
