65ac81d9884f500e6b6c35146576e6208bdfcdd4
Nada chaa <140740775+Nada-webdev@users.noreply.github.com>
2024-10-15 11:35:34 +0100

    update

--- paragraphs.js
+++ paragraphs.js
@@ -1,6 +1,19 @@
 const paragraphs = [
-          "Micro Club is a leading student organization at USTHB, founded in 1985, that fosters innovation and learning in technology and computer science. It offers students workshops, events, and project opportunities in areas like programming, web development, and more",
-          "Design Department: Focuses on creativity and visual communication, helping members master tools like Photoshop, Illustrator, and UI/UX design principles to create compelling graphics, user interfaces, and digital art for various projects.",
-          "Development (Dev) Department: Concentrates on software and web development, teaching members coding skills, best practices, and project development using modern programming languages and frameworks.",
-          "Game Development Department: Specializes in creating video games, where members explore game design, 3D modeling, and programming using platforms like Unity and Unreal Engine to build interactive gaming experiences."
-];
+          {
+            text: "micro club is a leading student organization at usthb, founded in 1985, that fosters innovation and learning in technology and computer science. it offers students workshops, events, and project opportunities in areas like programming, web development, and more.",
+            RoleColor: "rgb(66, 198, 255)"
+          },
+          {
+            text: "design department: focuses on creativity and visual communication, helping members master tools like photoshop, illustrator, and ui/ux design principles to create compelling graphics, user interfaces, and digital art for various projects.",
+            RoleColor: "aqua"
+          },
+          {
+            text: "development (dev) department: concentrates on software and web development, teaching members coding skills, best practices, and project development using modern programming languages and frameworks.",
+            RoleColor: "rgb(9, 61, 141)"
+          },
+          {
+            text: "game development department: specializes in creating video games, where members explore game design, 3d modeling, and programming using platforms like unity and unreal engine to build interactive gaming experiences.",
+            RoleColor: "rgb(122, 5, 122)"
+          }
+        ];
+        
--- script.js
+++ script.js
@@ -1,127 +1,107 @@
-const typingText=document.querySelector(".typing-text p")
-const inputFiled=document.querySelector(".wrapper .input-field")
-const mistakeTag=document.querySelector(".mistake span")
-const TimeTag=document.querySelector(".time span b")
-const TryAgainBtn=document.querySelector(".btn .again")
-const NextBtn=document.querySelector(".btn .next")
+const typingText = document.querySelector(".typing-text p");
+const inputFiled = document.querySelector(".wrapper .input-field");
+const mistakeTag = document.querySelector(".mistake span");
+const TimeTag = document.querySelector(".time span b");
+const TryAgainBtn = document.querySelector(".btn .again");
+const NextBtn = document.querySelector(".btn .next");
+const bodyBg = document.getElementsByTagName("body")[0]; 
 let time,
-timeLeft=timeMax=60
-let index=mistakes=StartTimer=0
-function randomPragraph(){
-          let randomIndex= Math.floor(Math.random()* paragraphs.length)
-          typingText.innerHTML="";
-         paragraphs[randomIndex].split("").forEach (letter=>{
-           let letterTag= `<span>${letter}</span>`
-           typingText.innerHTML +=letterTag
-         })
-         TimeTag.innerHTML=timeMax;
- typingText.addEventListener("click",()=>inputFiled.focus())
+    timeLeft = timeMax = 60,
+    index = i = mistakes = StartTimer = 0;
+let Timing;
 
+function randomPragraph() {
+  typingText.innerHTML = "";
+  paragraphs[i].text.split("").forEach(letter => {
+    let letterTag = `<span>${letter}</span>`;
+    typingText.innerHTML += letterTag;
+  });
+  TimeTag.innerHTML = timeMax;
+  bodyBg.style.backgroundColor = paragraphs[i].RoleColor; 
+  i = (i + 1) % paragraphs.length; 
+  typingText.querySelectorAll("span")[0].classList.add('active');
+  typingText.addEventListener("click", () => inputFiled.focus());
+  document.addEventListener('keydown',()=>inputFiled.focus());
 }
 
+function inputFun() {
+  const char = typingText.querySelectorAll("span");
+  let typedChar = inputFiled.value.split("")[index];
 
-function inputFun(){
-          const char = typingText.querySelectorAll("span");
-          let typedChar = inputFiled.value.split("")[index];
-          
-          if(timeLeft>0){
-            if(!StartTimer){
-              Timing= setInterval(initTime,1000);
-              StartTimer=true;
-            }
-            if(typedChar == null){
-              index--;
-              if(char[index].classList.contains("incorrect")){
-                mistakes --;
-              }
-              char[index].classList.remove("correct", "incorrect");
-            }else{
-  
-              if(char[index].innerText === typedChar){
-                char[index].classList.add("correct")
-      }else{
-               mistakes++;
-               
-                char[index].classList.add("incorrect")
+  if (timeLeft > 0) {
+    if (!StartTimer) {
+      Timing = setInterval(initTime, 1000);
+      StartTimer = true;
+    }
+    if (typedChar == null) {
+      index--;
+      if (char[index].classList.contains("incorrect")) {
+        mistakes--;
       }
-      index++
-            }
+      char[index].classList.remove("correct", "incorrect");
+    } else {
+      if (char[index].innerText === typedChar) {
+        char[index].classList.add("correct");
+      } else {
+        mistakes++;
+        char[index].classList.add("incorrect");
+      }
+      index++;
+    }
 
-            mistakeTag.innerHTML=mistakes;
-            char.forEach(span =>span.classList.remove("active"));
-            char[index].classList.add("active")
-          }else{
-            clearInterval(Timing);
-            inputFiled.value="";
-          }
-          
-         
-
-}
-
-function initTime(){
-  if(timeLeft >0){
-timeLeft--;
-TimeTag.innerText=timeLeft;
-  }
-  else{
-    clearInterval(Timing)
+    mistakeTag.innerHTML = mistakes;
+    char.forEach(span => span.classList.remove("active"));
+    char[index]?.classList.add("active");
+  } else {
+    clearInterval(Timing);
+    inputFiled.value = "";
   }
 }
 
-function TryAgain(){
-  
-  timeLeft=timeMax=60
-  index=mistakes=StartTimer=0
-  TimeTag.innerText=timeLeft;
-  mistakeTag.innerHTML=mistakes;
-  inputFiled.value="";
+function initTime() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    TimeTag.innerText = timeLeft;
+  } else {
+    clearInterval(Timing);
+  }
+}
+
+function TryAgain() {
+  timeLeft = timeMax = 60;
+  index = mistakes = StartTimer = 0;
+  TimeTag.innerText = timeLeft;
+  mistakeTag.innerHTML = mistakes;
+  inputFiled.value = "";
   clearInterval(Timing);
 
-
   const char = typingText.querySelectorAll("span");
-
   char.forEach(span => {
-    span.classList.remove("correct", "incorrect","active");
+    span.classList.remove("correct", "incorrect", "active");
   });
-  
-
-
 }
 
-
 function Next() {
-  let finishedAllText = true; 
-  
+  let finishedAllText = true;
+
   const char = typingText.querySelectorAll("span");
-  
-  
   char.forEach(span => {
     if (!span.classList.contains("correct") &&
-        !span.classList.contains("incorrect")) {
-      finishedAllText = false;  
+      !span.classList.contains("incorrect")) {
+      finishedAllText = false;
     }
   });
 
-  
- 
   if (timeLeft > 0 && finishedAllText) {
-   
     timeLeft = timeMax = 60;
     index = mistakes = StartTimer = 0;
-
-    
     TimeTag.innerText = timeLeft;
     mistakeTag.innerHTML = mistakes;
     inputFiled.value = "";
-
-  
     clearInterval(Timing);
 
-    
     randomPragraph();
   } else {
-    
     console.log("You must finish typing the text or time has run out.");
   }
 }
